e85b8c5d42ce5cbd8522a6d6d8a01ff3183bc8df
wwesleyalves <97242196+wwesleyalves@users.noreply.github.com>
2026-01-15 19:27:45 -0300

    Update secretsGate.groovy

--- vars/secretsGate.groovy
+++ vars/secretsGate.groovy
@@ -3,10 +3,6 @@ def call(Map config = [:]) {
     String report     = config.get('report', 'cx_result.json')
     int maxAgeDays    = config.get('maxAgeDays', 10)
 
-    echo "Executando Policy Gate - Secrets"
-    echo "Relatório: ${report}"
-    echo "Idade máxima permitida: ${maxAgeDays} dias"
-
     writeFile(
         file: 'gate_secrets.sh',
         text: libraryResource('gate_secrets.sh')
